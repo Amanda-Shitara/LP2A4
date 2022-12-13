03c958497c624196732ee4b39e63e168c9465abc
Amanda-Shitara <69778375+Amanda-Shitara@users.noreply.github.com>
2022-12-13 11:21:19 -0300

    Delete Atividades/N1/target directory

--- Atividades/N1/target/classes/META-INF/sql/inserir_dados.sql
+++
@@ -1,9 +0,0 @@
-insert into proprietario (id, nome, email) values (1, 'Rayssa Pietra Melo', 'rayssa.melo@gmail.com');
-insert into proprietario (id, nome, email) values (2, 'Ana Carolina Lima', 'carol_lima@outlook.com');
-insert into proprietario (id, nome, email) values (3, 'Thales dos Santos', 'thales_santos@uol.com.br');
- 
-insert into veiculo (id, fabricante, modelo, proprietario_id) values (1, 'Chevrolet', 'Onix', 1);
-insert into veiculo (id, fabricante, modelo, proprietario_id) values (2, 'Volkswagen', 'Gol', 2);
-insert into veiculo (id, fabricante, modelo, proprietario_id) values (3, 'Fiat', 'Uno', 2);
-insert into veiculo (id, fabricante, modelo, proprietario_id) values (4, 'Hyundai', 'Creta', 3);
-insert into veiculo (id, fabricante, modelo, proprietario_id) values (5, 'Renault', 'Sandero', 3);
